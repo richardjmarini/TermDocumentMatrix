f1f3b15289c1079447e048d80e462a01270f2c1e
Richard J. Marini <marini@motif.(none)>
2014-03-31 09:35:30 -0400

    added setup.py for install

---
+++ src/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup, Extension
+
+setup(
+   name= 'Vector Space Search Engine',
+   version= '1.0',
+   description= 'Vector Space Search Engine',
+   author= 'Richard J. Marini',
+   author_email= 'richardjmarini@gmail.com',
+   url= 'https://github.com/richardjmarini/VectorSpaceSearchEngine',
+   long_description= '''
+      Vector Space Search Engine
+   ''',
+   py_modules= ['vspse']
+)
+
