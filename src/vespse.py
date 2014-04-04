5600b44b95d7526e1fca0bfebf984f5547a4645d
Richard J. Marini <richardjmarini@gmail.com>
2014-04-04 17:43:30 -0400

    fixed bug when creating a document and passing in id= 0 -- if id would create hexdigest id instead of using id 0

--- src/vespse.py
+++ src/vespse.py
@@ -83,15 +83,16 @@ class Document(object):
 
    #stopwords= ("of", "in", "a", "the", "got", "into", "is", "it", "and", "-", ",", ".", "\n")
    stopwords= stopwords.words('english')
-
+  
 
    def __init__(self, text, id= None, stopwords= []):
 
       super(Document, self).__init__()
 
       self.text= text
-      self.id= id if id else md5(text).hexdigest() 
-      self.stopwords+= stopwords
+      self.id= id if id != None else md5(text).hexdigest() 
+      self.stopwords+= stopwords + ['aly', 'alley', 'arc', 'arcade', 'ave', 'avenue', 'bch', 'beach', 'bg', 'burg', 'bgs', 'burgs', 'blf', 'bluff', 'blvd', 'boulevard', 'bnd', 'bend', 'br', 'branch', 'brg', 'bridge', 'brk', 'brook', 'brks', 'brooks', 'btm', 'bottom', 'byp', 'bypass', 'byu', 'bayou', 'cir', 'circle', 'cirs', 'circles', 'clb', 'club', 'clf', 'cliff', 'clfs', 'cliffs', 'cmn', 'common', 'cor', 'corner', 'cors', 'corners', 'cp', 'camp', 'cpe', 'cape', 'cres', 'crescent', 'crk', 'creek', 'crse', 'course', 'crst', 'crest', 'cswy', 'causeway', 'ct', 'court', 'ctr', 'center', 'ctrs', 'centers', 'cts', 'courts', 'curv', 'curve', 'cv', 'cove', 'cvs', 'coves', 'cyn', 'canyon', 'dl', 'dale', 'dm', 'dam', 'ln', 'lane', 'rd', 'road', 'st', 'street', 'terr', 'terrace', 'xing', 'crossing'] + ['academy', 'acad.', 'association', 'assn.', 'assn', 'associates', 'assoc.', 'college', 'coll.', 'company', 'co.', 'corporation', 'corp.', 'doing business as', 'd.b.a.', 'dba', 'incorporated', 'inc.', 'institute/institution', 'inst.', 'limited', 'ltd.', 'ltd', 'limited liability company (or partnership)', 'llc', 'llc (llp)', 'public limited company', 'plc', 'manufacturing', 'mfg.', 'mfg', 'publishing', 'pubg.', 'pubg', 'publications', 'pub., pubs., pubs', 'press', 'pr.', 'university', 'univ.', 'u.', 'uni.']
+
 
       stemmer= PorterStemmer()
       self.tokens= filter(lambda word: stemmer.stem(word.lower()).lower() not in self.stopwords,  word_tokenize(self.text))
@@ -109,7 +110,7 @@ class TermDocumentMatrix(dict):
 
    def add(self, text, id= None):
      
-      document= Document(text, id)
+      document= Document(text, id= id)
 
       self[document.id]= document
 
